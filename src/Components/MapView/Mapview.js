978952f0880a0320cae6a8b7faa000e185cb122b
Alexandra Contreras Hernández <alexconh@192.168.0.159>
2022-04-16 15:16:01 -0500

    UPDATE AddPosition

--- src/Components/MapView/Mapview.js
+++ src/Components/MapView/Mapview.js
@@ -1,7 +1,7 @@
-import { React, useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import { React, useEffect, useState } from "react"
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import L from "leaflet"
 import "./MapView.css"
 
 const icon = L.icon({
@@ -10,51 +10,43 @@ const icon = L.icon({
   popupAnchor: [2, -40],
   iconUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png"
-});
+})
 
 function MapView() {
   function LocationMarker() {
-    const [position, setPosition] = useState(null);
-    const [bbox, setBbox] = useState([]);
+    const [position, setPosition] = useState(null)
+    const [bbox, setBbox] = useState([])
 
-    const map = useMap();
+    const map = useMap()
 
     useEffect(() => {
       map.locate().on("locationfound", function (e) {
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-        const radius = e.accuracy;
-        const circle = L.circle(e.latlng, radius);
-        circle.addTo(map);
-        setBbox(e.bounds.toBBoxString().split(","));
-      });
-    }, [map]);
+        setPosition(e.latlng)
+        map.flyTo(e.latlng, map.getZoom())
+        const radius = e.accuracy
+        const circle = L.circle(e.latlng, radius)
+        circle.addTo(map)
+        setBbox(e.bounds.toBBoxString().split(","))
+      })
+    }, [map])
 
     return position === null ? null : (
       <Marker position={position} icon={icon}>
         <Popup>
           <b className="position-popup">Usted está aquí</b>
-         {/*  You are here. <br />
-          Map bbox: <br />
-          <b>Southwest lng</b>: {bbox[0]} <br />
-          <b>Southwest lat</b>: {bbox[1]} <br />
-          <b>Northeast lng</b>: {bbox[2]} <br />
-          <b>Northeast lat</b>: {bbox[3]} */}
         </Popup>
       </Marker>
-    );
+    )
   }
 
   return (
     <MapContainer
       center={[19.390734,-99.143613]}
       zoom={16}
-      scrollWheelZoom
-    >
+      scrollWheelZoom >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <LocationMarker />
     </MapContainer>
   );
--- src/Views/AddPosition/AddPosition.js
+++ src/Views/AddPosition/AddPosition.js
@@ -23,20 +23,21 @@ const AddPosition = () => {
                 <NavBar />
                 <h1 className="title ms-4">Elige tu ubicación</h1>
             </div>
-            <div className="add-position-body">
-                <form className="mx-4">
+            <div className="add-position-body px-4">
+                <form>
                     <div className="add-position-input">
                         <Search color="#666666" width={'20px'} className="mx-1"/>
                         <input type="search" placeholder='Calle #, Colonia, Ciudad, Estado' className="mx-1" onChange={addressHandler}/>
                     </div>
                 </form>
-                <div className="map-container mx-4">
+                <div className="map-container">
                     <MapView></MapView>
                 </div>
-                <p className="current-position ms-4">Usar ubicación actual</p>
+                <p className="current-position">Usar ubicación actual</p>
+                <textarea placeholder="Referencias adicionales" name="more details" id="position-details" rows="3"></textarea>
             </div>
-            <div className="add-position-footer">
-                <Button width={'medium'} color="primary">Guardar</Button>
+            <div className="add-position-footer mx-4">
+                <Button width={'large'} color="primary">Guardar</Button>
             </div>
         </section>
     )
