2d77b3ccef67fb24f0ab11d5904a13403544044a
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2022-04-18 14:07:25 -0500

    despliegue de coches por usuario en las cards con sus respectivos valores

--- src/Views/MyVehicles/MyVehicles.js
+++ src/Views/MyVehicles/MyVehicles.js
@@ -6,13 +6,14 @@ import CardVehicles from "../../Components/CardVehicles/CardVehicles";
 import NavBar from "../../Components/NavBar/NavBar";
 import Button from "../../Components/Button/Button"
 
-function MyVehicles(props) {
+function MyVehicles() {
   
+  const [carData, setCarData] = useState()
   
   const token = localStorage.getItem('token')
   const userId = localStorage.getItem('user')
-  console.log(token)
-  console.log(userId)
+  // console.log(token)
+  // console.log(userId)
   
   if (!token) {
       return <Navigate to="/login" replace />;
@@ -21,7 +22,7 @@ function MyVehicles(props) {
     const getVehicles = () => {
       axios.get(`http://localhost:4000/cars/cars/${userId}`)
       .then(res => {
-        console.log(res)
+        setCarData(res.data.allCars)
       })
       .catch(e =>{
         console.error(e)
@@ -34,19 +35,17 @@ function MyVehicles(props) {
       <NavBar />
       <section>
         <h1 className='title'>Mis Vehículos</h1>
-        {/* <CardVehicles
-          brand={brand}
-          model={model}
-          color={color} />
-
-        {myVehicles.map((vehicle, index) => {
-          return <CardVehicles
-            key={index}
-            model={vehicle.model}
-            brand={vehicle.brand}
-            color={vehicle.color}
-          />
-        })} */}
+        {carData  && carData.map((car, index) => {
+          const {marca, modelo, placa, color} = car
+            return(
+                <CardVehicles
+                key={index}
+                brand={marca}
+                model={modelo}
+                color={color}
+                licensePlate={placa} />
+              )
+          })}
       </section>
       <Button color="primary" width="large" onClick={() => getVehicles()}>Añadir</Button>
     </div>
