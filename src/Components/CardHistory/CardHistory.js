66024ca1656ddf7abe4cb4b4e367bba4fb7ef24a
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2022-04-22 18:31:21 -0500

    imagenes de card

--- src/Components/CardHistory/CardHistory.js
+++ src/Components/CardHistory/CardHistory.js
@@ -1,13 +1,35 @@
 import React from "react"
-import jeep from "../../Images/jeep-min.svg"
 import "./CardHistory.css"
 import { MapPin, Calendar } from "react-feather"
+import Crossover from "../Crossover";
+import Moto from "../Motorcicle";
+import Suv from "../Suv";
+import Sedan from "../Sedan";
+import Hatchback from "../Hatchback";
+import Pickup from "../Pickup";
+import Van from "../Van";
 
-function CardHistory({ service, brand, model, location, date, time }) {
+function CardHistory({ service, brand, model, location, date, time, vehiculo }) {
+
+const getCarType = () => {
+  switch(vehiculo){
+    case 'motocicleta': return <Moto stroke={'black'} />;
+    case 'crossover': return <Crossover stroke={'black'} />;
+    case 'sedan': return <Sedan stroke={'black'} />;
+    case 'hatchback': return <Hatchback stroke={'black'} />;
+    case 'suv': return <Suv stroke={'black'} />;
+    case 'pickup': return <Pickup stroke={'black'} />;
+    case 'van': return <Van stroke={'black'} />;
+    default: return <Sedan stroke={'black'} />;
+  }
+ }
+
+
+
   return (
     <div className="card-history d-flex align-items-center">
       <div className="card-history-img">
-          <img src={jeep} alt="Imagen vehículo" />
+          <img src={getCarType()} alt="Imagen vehículo" />
       </div>
       <div className="card-history-info">
         <p className="history-brand">{service} • {brand} {model}</p>
